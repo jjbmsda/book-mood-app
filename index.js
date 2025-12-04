1a3c2ae2937df72ec701117d0042cd3ce3db148e
jjbmsda <jjbmsda@naver.com>
2025-12-04 14:01:02 +0900

    modify

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 
 registerRootComponent(App);
-// 내부에서 AppRegistry.registerComponent('main', () => App) 를 대신 호출해줌..
+// 내부에서 AppRegistry.registerComponent('main', () => App) 를 대신 호출해줌.....
